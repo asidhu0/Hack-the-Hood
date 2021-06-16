07cba173bac9e95980dcf5aebf135f53d65133b0
Avnoor Singh Sidhu <Avnoor@MacBook-Air.local>
2021-06-16 12:47:04 -0700

    adding files

---
+++ fund_prac.py
@@ -0,0 +1,17 @@
+print("Hello World")
+name = "Lebron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+car_name = "Ford"
+x = 50
+my_first_name = "Avnoor"
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
+print(type(car_name))
+print(type(x))
+print(type(my_first_name))
